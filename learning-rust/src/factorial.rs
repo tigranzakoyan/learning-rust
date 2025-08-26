75901a1ec3419624cc08f9e5ee90349c5e48a810
tigran.zakoyan <dqzakoyan@gmail.com>
2025-08-26 08:47:46 +0100

    refactor factorial module

--- learning-rust/src/factorial.rs
+++ learning-rust/src/factorial.rs
@@ -1,14 +1,14 @@
 pub fn factorial_rec(n: u64) -> u64 {
-    if n == 1 {
-        return n
+    if n == 0 {
+        return 1
     }
     factorial_rec(n-1) * n
 }
 
 pub fn factorial_loop(n: u64) -> u64 {
     let mut sum = 1;
-    for m in 1..n+1 {
-        sum *= m;
+    for m in 2..n+1 {
+        sum *= m
     }
     sum
 }
@@ -20,13 +20,15 @@ mod tests {
 
     #[test]
     fn test_factorial_rec() {
-        let result = factorial_rec(5);
-        assert_eq!(result, 2 * 3 * 4 * 5);
+        assert_eq!(factorial_rec(0), 1);
+        assert_eq!(factorial_rec(1), 1);
+        assert_eq!(factorial_rec(5), 2 * 3 * 4 * 5);
     }
 
     #[test]
     fn test_factorial_loop() {
-        let result = factorial_loop(5);
-        assert_eq!(result, 2 * 3 * 4 * 5);
+        assert_eq!(factorial_loop(0), 1);
+        assert_eq!(factorial_loop(1), 1);
+        assert_eq!(factorial_loop(5), 2 * 3 * 4 * 5);
     }
 }
